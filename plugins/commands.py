41db005e453f165bfeb3de6d20ad94bed8282260
TMCreation <87004839+TMCreation@users.noreply.github.com>
2021-11-16 12:32:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,7 +17,7 @@ async def start(bot, message):
         buttons = [[
             InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Go Inline', switch_inline_query=''),
-            InlineKeyboardButton('📽 🄲🄸🄽🄴🄷🅄🄱', @cinehub_family=''),
+            InlineKeyboardButton('📽 🄲🄸🄽🄴🄷🅄🄱', switch_inline_query=''),
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(START_MSG, reply_markup=reply_markup)
