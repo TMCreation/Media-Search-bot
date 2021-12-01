6a3bc1931a3bdeb9dc365890f5609bdd1bd8796c
TMCreation <87004839+TMCreation@users.noreply.github.com>
2021-12-01 14:39:41 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -74,7 +74,7 @@ def get_reply_markup(username, query):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
     buttons = [[
         InlineKeyboardButton('🔎 sᴇᴀʀᴄʜ ᴀɢᴀɪɴ', switch_inline_query_current_chat=query),
-        InlineKeyboardButton("🎞 සිංහල උපසිරැසි", url="https://t.me/sub_searcher_bot"),
+        InlineKeyboardButton("🎞 උපසිරැසි", url="https://t.me/sub_searcher_bot"),
         InlineKeyboardButton('🔄 sʜᴀʀᴇ ʙᴏᴛ', url=url),
     ],[
         InlineKeyboardButton('🎬 ᴄʟɪᴄᴋ ᴛᴏ sᴇᴀʀᴄʜ ᴍᴏᴠɪᴇ ᴏʀ ᴛᴠ sᴇʀɪᴇs', switch_inline_query_current_chat=''),
